5db22644202f6ba38a6c6b957c1dd0805adb16f6
decozzfx <m.f.r.ozil@gmail.com>
2024-12-14 12:55:11 +0700

    feat(general): progress animation bar

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Main from "../components/layouts/Main";
 import theme from "../libs/theme";
 import Fonts from "../components/Fonts";
 import { AnimatePresence } from "framer-motion";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps, router }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps, router }) {
       <Fonts />
       <Main router={router}>
         <AnimatePresence mode="popLayout" initial={true}>
+          <NextNProgress />
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Main>
