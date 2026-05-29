7da3f08ddb3d9200886105d858c67d34d900e251
mochfathurrozi <decozzfx@gmail.com>
2026-05-29 22:49:06 +0700

    feat: lenis smooth scroll synced with gsap scrolltrigger

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,21 +1,20 @@
 import { Box, Container, Flex } from '@chakra-ui/react'
 import { Navbar } from '@/components/layout/navbar'
 import { Footer } from '@/components/layout/footer'
+import { SmoothScroll } from '@/components/motion/smooth-scroll'
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Flex direction="column" minH="100vh">
-      <Navbar />
-      <Box as="main" pt="80px" flex="1">
-        <Container maxW="container.md" py={8}>
-          {children}
-        </Container>
-      </Box>
-      <Footer />
-    </Flex>
+    <SmoothScroll>
+      <Flex direction="column" minH="100vh">
+        <Navbar />
+        <Box as="main" pt="80px" flex="1">
+          <Container maxW="var(--container)" py={8}>
+            {children}
+          </Container>
+        </Box>
+        <Footer />
+      </Flex>
+    </SmoothScroll>
   )
 }
