a57547e3165d18fcdf12715d57cd5b5f109d8c0b
mochfathurrozi <decozzfx@gmail.com>
2026-05-30 00:11:18 +0700

    fix: center main content container (chakra Container was left-pinned)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { SmoothScroll } from "@/components/motion/smooth-scroll";
@@ -15,9 +15,9 @@ export default function MainLayout({
       <Flex direction="column" minH="100vh">
         <Navbar />
         <Box as="main" pt="80px" flex="1">
-          <Container maxW="var(--container)" py={8}>
+          <Box mx="auto" w="100%" maxW="var(--container)" px="1.5rem" py={8}>
             {children}
-          </Container>
+          </Box>
         </Box>
         <Footer />
       </Flex>
