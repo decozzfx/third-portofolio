34a9635f27503399d803e28f72052cc8a04bac56
mochfathurrozi <decozzfx@gmail.com>
2026-05-29 22:53:26 +0700

    feat: add ShinyCTA button component

---
+++ components/motion/shiny-cta.tsx
@@ -0,0 +1,11 @@
+import styles from './shiny-cta.module.css'
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & { children: React.ReactNode }
+
+export function ShinyCTA({ children, className = '', ...rest }: Props) {
+  return (
+    <button className={`${styles.cta} ${className}`} {...rest}>
+      {children}
+    </button>
+  )
+}
